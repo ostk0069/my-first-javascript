759f974c1033639090d7e34de91bc1edb748dbc9
Takuma Osada <judgment9ttk069@gmail.com>
2017-09-22 20:48:18 +0900

    finish chapter3

---
+++ main.js
@@ -0,0 +1,82 @@
+// $ (document).ready(function() {
+//   'use strict';
+  // let currentTempC = 16;
+  // console.log(currentTempC);
+  // currentTempC = 22.4
+  // console.log(currentTempC);
+
+  // paper.install(window);
+  // paper.setup(document.getElementById('mainCanvas'));
+
+  /* write here to draw */
+
+  // let c;
+  // for(let x=25; x<400; x+=50) {
+  //   for(let y=25; y<400; y+=50) {
+  //     c = Shape.Circle(x, y, 20);
+  //     c.fillColor = 'green';
+  //   }
+  // }
+
+  // let tool = new Tool();
+
+  // tool.onMouseDown = function(event) {
+  //   let c = Shape.Circle(event.point.x, event.point.y, 20);
+  //   c.fillColor = 'green';
+  // };
+
+  //Hello World!
+  // let c = Shape.Circle(200, 200, 80);
+  // c.fillColor = 'black';
+  // let text = new PointText(200, 200);
+  // text.justification = 'center';
+  // text.fillColor = 'white';
+  // text.fontSize = 20;
+  // text.content = 'hello world';
+
+
+  // paper.view.draw();
+  // const obj = {};
+  // obj.color = "黄色";
+  // obj["not an identifier"] = 3;
+  // console.log(obj["not an identifier"]);
+  // console.log(obj.color);
+  // console.log(obj["color"]); //[color]は反応しない
+
+  // const obj = {};
+  // obj.color = "黄色";
+  // obj["not an identifier"] = 3;
+  // console.log(obj["not an identifier"]);
+  //
+  // const SIZE = Symbol(); //大文字じゃないと反応しない
+  // obj[SIZE] = 8;
+  // console.log(obj[SIZE]);
+  // console.log(obj);
+
+  // const obj = {};
+  // const SIZE = Symbol();
+  // obj[SIZE] = 8;
+  // console.log(obj[SIZE]);
+  // console.log(obj);
+  //
+  // obj.SIZE = 12;
+  // console.log(obj.SIZE);
+  // console.log(obj["SIZE"]);
+  // console.log(obj[SIZE]);
+  // console.log(obj);
+/*difference between object and perimittive
+obj had been read the same state. yet the perimittive has changed some times.
+object wont be error any times
+
+non and [""] is the same meaning*/
+
+  // sam3.speak = function() {return "Meow!"; };
+
+  // tama.cry = function() { return "ニャーゴ！"; };
+
+  // console.log(sam3.speak());
+  //
+  // delete sam3.classification;
+
+
+// });
---
+++ support.js
@@ -0,0 +1,74 @@
+$ (document).ready(function() {
+  'use strict';
+  // const s = "hello";
+  // // const s2 = s.toUpperCase(); //exchange s2 to s
+  // // console.log(s2);
+  //
+  // s.rating = 3;
+  // console.log(s.rating);//exchange only for a moment
+
+  const a1 = [1, 2, 3, 4];
+  const a2 = [1, 'two', 3, null];
+  const a3 = [
+    "what the hammer?",
+    "In what furnace was thy brain?",
+    "what the anvil? what dread grasp",
+    "Dare its deadly terrors clasp?",
+  ];
+  const a4 = [
+    { name: "Ruby", hardness: 9 },
+    { name: "Python", hardness: 12 },
+    { name: "Javascript", hardness: 10 },
+  ];
+  const a5 = [
+    [1, 2, 3],
+    [2, 4, 6],
+  ];
+
+  // const a = ['a', 'b', 'c'];
+  // console.log(a.length);
+  // console.log(a[0]);
+  // console.log(a[a.length -1]);
+  //
+  // console.log(a4[1].name);
+
+  // const now = new Date();
+  // console.log(now);
+  //
+  // const numStr = "33.3";
+  // const num = Number(numStr);//数値へ変換
+  //
+  // // console.log(num);
+  //
+  // const a = parseInt("16 volts", 10);//do not count volts //16
+  // const b = parseInt("3a", 16);
+  // console.log(b);
+  // const c = parseInt("15.5 kph");
+  // console.log(c);
+  //
+  // const d = new Date();
+  // console.log(d);
+  // const ts = d.valueOf(); //more info
+  // console.log(ts);
+  //
+  // const w = true;
+  // const s = false;
+  // const n = w ? 1 : 0;
+  // console.log(n);
+  // const m = s ? 1 : 0;
+  // console.log(m);
+
+  // const t = 33.5;
+  // const h = t.toString();//文字列へ変換
+  //
+  // console.log(h);
+  /*それほど便利なものではない。数値から文字列への変化を行うが、凍結したり、
+  テンプレートリテラルを使うときに必要はない。配列には非常に便利。*/
+
+  const n = 0;
+  const b0 = !n;
+  const b1 = !!n;
+  const b2 = Boolean(n);
+  // ! = disagreement
+
+});
