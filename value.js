e8e1fc4c33176f8f0c7d6e37563148ef79050952
Takuma Osada <judgment9ttk069@gmail.com>
2017-09-25 20:18:01 +0900

    finish ep5

---
+++ value.js
@@ -0,0 +1,72 @@
+// const s = "5";
+// console.log(3 + +s);
+// console.log(3 + s);
+//
+// const x1 = 0, x2 = 3, x3 = -1.5, x4 = -6.33;
+// console.log(-x1*1);
+// console.log(+x2*2);
+// console.log(+x3*3);
+// console.log(-x4*4);
+//
+// console.log(10%-4);
+// let x = 2;
+// console.log(x++ + x++);
+// console.log(++x + ++x);
+// console.log(x++ + ++x);
+// console.log(++x + x++);
+// console.log(x);
+// console.log("-----");
+// let y = 10;
+// console.log(y-- + y--);
+// console.log(--y +  --y);
+// console.log(y-- + --y);
+// console.log(--y + y--);
+// console.log(y);
+//
+// let x = 3; y;
+// x += y = (6*5)/2
+
+// const n = 5;
+// const s = "5";
+// console.log(n === s);
+// console.log(n !== s);
+// console.log(n === Number(s));
+// console.log(n !== Number(s));
+// console.log(n == s);
+// console.log(n != s);
+//
+//
+// console.log(NaN === NaN);
+// console.log(NaN == NaN);
+// console.log(isNaN(NaN));//true
+// console.log(isNaN(3));
+// console.log(isNaN("abc"));
+//
+// let x = 0;
+// while(true) {
+//   x += 0.1;
+//   console.log(x);
+//   if(x === 0.3) break;
+// }
+// console.log(`${x}`);
+// let x = 0;
+// while(true) {
+//   x += 0.1;
+//   console.log(x);
+//   if(Math.abs(x - 0.3) < Number.EPSILON) break;
+// }
+// console.log(`${x}`);
+
+if ('') console.log("t"); else console.log("f");
+
+// const skipIt = true;
+// let x = 0;
+// const result = skipIt || x++;
+//
+// const doIt = false;
+// let x = 0;
+// const result = doIt && x++;
+//
+// // same meaning
+//
+// const options = suppliedOptions || { name: "default"}
